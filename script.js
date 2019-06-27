8e7f1f64f7ee91a6b703e482ef14039a74d866e9
i. dos santos <iosorio.santos@gmail.com>
2019-06-27 17:10:05 -0300

    added popup when mouse is leaving the site

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+/**
+ * Config Popup
+ */
+var mouseX = 0;
+var mouseY = 0;
+var popupOpened = false;
+
+document.addEventListener("mousemove", ev => {
+  mouseX = ev.clientX;
+  mouseY = ev.clientY;
+
+  if (mouseY < 60 && !popupOpened) {
+    popupOpened = true;
+
+    const popup = document.getElementById("popup");
+    popup.style["display"] = "inline-block";
+    const popupDivBounding = document
+      .getElementById("popupContainer")
+      .getBoundingClientRect();
+    const closePopup = document.getElementById("closePopup");
+
+    const closePopupFn = () => {
+      popup.style["display"] = "none";
+    };
+
+    popup.addEventListener("click", ev => {
+      const x = ev.clientX;
+      const y = ev.clientY;
+
+      if (
+        (x < popupDivBounding.left || x > popupDivBounding.right) ||
+        (y < popupDivBounding.top || y > popupDivBounding.bottom)
+      ) {
+        closePopupFn();
+      }
+    });
+
+    closePopup.addEventListener("click", ev => {
+      closePopupFn();
+    });
+  }
+});
+
+/**
+ * Config Counter
+ */
 const dayEl = document.getElementById("dayCounter");
 const hourEl = document.getElementById("hourCounter");
 const minuteEl = document.getElementById("minuteCounter");
@@ -8,7 +54,7 @@ const launchDate = new Date(2019, 11, 25, 0, 0, 0).getTime();
 setTime();
 
 setInterval(() => {
-    setTime();
+  setTime();
 }, 1000);
 
 function setTime() {
